973fa5244ae3ca2f9e776e70f4da990520745915
Liu Bo <diablo47@gmail.com>
2014-10-31 00:20:23 +0800

    recursive function

--- ctci-excercise/src/chapter2/Q2_7.java
+++ ctci-excercise/src/chapter2/Q2_7.java
@@ -2,6 +2,19 @@ package chapter2;
 
 import java.util.Stack;
 
+class Result<T> {
+	public ListNode<T> node;
+	public boolean result;
+
+	public Result() {
+	}
+
+	public Result(ListNode<T> t, boolean result) {
+		this.node = t;
+		this.result = result;
+	}
+}
+
 public class Q2_7 {
 
 	public static <T> ListNode<T> reverse(ListNode<T> list) {
@@ -39,6 +52,28 @@ public class Q2_7 {
 		return true;
 	}
 
+	public static <T> Result<T> isMirror2(ListNode<T> list, int length) {
+		Result<T> temp = null;
+		if (list == null || length == 0) {
+			return new Result(null, true);
+		}
+		if (length == 1) {
+			temp = new Result<T>(list.next, true);
+			return temp;
+		}
+		if (length == 2) {
+			temp = new Result<T>();
+			temp.node = list.next.next;
+			temp.result = list.data.equals(list.next.data);
+			return temp;
+		}
+		temp = isMirror2(list.next, length - 2);
+		boolean compare = list.data.equals(temp.node.data);
+		temp.result = compare && temp.result;
+		temp.node = temp.node.next;
+		return temp;
+	}
+
 	public static void main(String[] args) {
 		ListNode<Integer> list = null;
 		ListNode<Integer> head = list;
@@ -47,7 +82,7 @@ public class Q2_7 {
 		float d = 3 / 2;
 		System.out.println(d);
 		System.out.println(Math.ceil(d));
-		int n = 5;
+		int n = 99;
 		int middle = n % 2 == 0 ? n / 2 : n / 2 + 1;
 		boolean isEven = n % 2 == 0;
 		for (int i = 1; i <= n; i++) {
@@ -70,5 +105,6 @@ public class Q2_7 {
 
 		System.out.println(list.printList());
 		System.out.println(isMirror(list));
+		System.out.println(isMirror2(list, n).result);
 	}
 }
